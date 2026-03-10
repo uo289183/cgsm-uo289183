72117d5d524aef92f40e596661872e4b827200db
Pablo González García <pablogonzalezgarcia003@gmail.com>
2026-03-10 16:43:31 +0100

    Sesion1: Implementación tarea 2.

---
+++ sesion1/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    mode: "development",
+    entry: {
+        "prac1-1": "./src/prac1-1.js",
+        "prac1-2": "./src/prac1-2.js"
+    },
+    output: {
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                type: 'javascript/auto',
+                resolve: {
+                    fullySpecified: false
+                }
+            }
+        ]
+    }
+};
