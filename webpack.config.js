16c077922503f98d7bec7fad9b654cb864e37f07
Pablo González García <pablogonzalezgarcia003@gmail.com>
2026-03-10 16:17:47 +0100

    Sesion1: Commit incial.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    mode: "development",
+    entry: {
+    },
+    output: {
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: __dirname
+        },
+        devMiddleware: {
+            writeToDisk: true
+        }
+    },
+    performance: {
+        maxAssetSize: 1000000,
+        maxEntrypointSize: 1000000
+    },
+    module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        type: 'javascript/auto',
+        resolve: {
+          fullySpecified: false
+        }
+      }
+    ]
+  }
+};
