6a29fc055dfaf19df1521d890d184bc4f8a2a92e
Pablo González García <pablogonzalezgarcia003@gmail.com>
2026-03-24 12:29:25 +0100

    Sesion2: Implementación tarea final (report).

--- sesion2/webpack.config.js
+++ sesion2/webpack.config.js
@@ -1,6 +1,15 @@
 module.exports = {
     mode: "development",
     entry: {
+        "prac2-1": "./src/prac2-1.js",
+        "prac2-2": "./src/prac2-2.js",
+        "prac2-3": "./src/prac2-3.js",
+        "prac2-4": "./src/prac2-4.js",
+        "prac2-5": "./src/prac2-5.js",
+        "prac2-6": "./src/prac2-6.js",
+        "prac2-7": "./src/prac2-7.js",
+        "prac2-8": "./src/prac2-8.js",
+        "prac2-9": "./src/prac2-9.js"
     },
     output: {
         filename: '[name].js'
@@ -18,14 +27,20 @@ module.exports = {
         maxEntrypointSize: 1000000
     },
     module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        type: 'javascript/auto',
-        resolve: {
-          fullySpecified: false
-        }
-      }
-    ]
-  }
+        rules: [
+            {
+                test: /\.m?js$/,
+                type: 'javascript/auto',
+                resolve: {
+                    fullySpecified: false
+                }
+            },
+            {
+                test: /\.glsl$/,
+                use: {
+                    loader: 'webpack-glsl-loader'
+                }
+            }
+        ]
+    }
 };
